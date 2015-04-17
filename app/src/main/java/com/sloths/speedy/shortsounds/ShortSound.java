6b1c6e07058f4242504cbfb80f7fc8d39781c729
Neil <nharlow@nulytiq.com>
2015-04-17 12:53:03 -0700

    started on method stubs

---
+++ app/src/main/java/com/sloths/speedy/shortsounds/ShortSound.java
@@ -0,0 +1,59 @@
+package com.sloths.speedy.shortsounds;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class ShortSound {
+
+    /**
+     * Local vars
+     */
+    private List<ShortSoundTrack> tracks;
+    private String title;
+
+
+    public ShortSound() {
+        // Create entry in DB
+    }
+
+    public static void getAll() {
+
+    }
+
+    public static void get( String identifier ) {
+
+    }
+
+    /**
+     * Save the ShortSound to disk (does not generate compiled audio file)
+     */
+    public void save() {
+
+    }
+
+    /**
+     * Generate audio file
+     */
+    public void generateAudioFile() {
+
+    }
+
+    public void getTracks() {
+
+    }
+    public void addTrack() {
+
+    }
+    public void removeTrack() {
+
+    }
+
+    public void getTitle() {
+
+    }
+    public void setTitle() {
+
+    }
+}
--- app/src/main/java/com/sloths/speedy/shortsounds/ShortSoundTrack.java
+++ app/src/main/java/com/sloths/speedy/shortsounds/ShortSoundTrack.java
@@ -3,5 +3,6 @@ package com.sloths.speedy.shortsounds;
 /**
  * Created by neilharlow on 4/17/15.
  */
-public class ShortSoundModel {
+public class ShortSoundTrack {
+
 }
