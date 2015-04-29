82ada560f36d31158aadd476df7c2f907586dc78
sigotron <sigotron@gmail.com>
2015-04-29 16:28:53 -0700

    Added some dependencies for AndroidJUnitRunner and created a test that fails to satisfy our continuous build integration server requirement.

---
+++ app/src/androidTest/java/com/sloths/speedy/shortsounds/InitialFailingTest.java
@@ -0,0 +1,34 @@
+package com.sloths.speedy.shortsounds;
+
+/**
+ * Created by joel on 4/29/2015.
+ */
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityUnitTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class InitialFailingTest extends ActivityUnitTestCase<MainActivity> {
+
+    public InitialFailingTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /**
+     * Creating a failing test in order to satisfy the zero-feature requirement
+     * that we have a continuous integration builder server set up that can send
+     * emails to team members when a build fails.
+     */
+    @Test
+    public void initialFailingTestForJenkins() {
+        assertTrue(false);
+    }
+
+}
