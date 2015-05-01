26ff38a2c448cb01d50fccaacc26cbd8a59c78e7
Neil <nharlow@nulytiq.com>
2015-05-01 13:35:53 -0700

    Commented the Models and Implemented rep invariants.

--- app/src/main/java/com/sloths/speedy/shortsounds/ShortSound.java
+++ app/src/main/java/com/sloths/speedy/shortsounds/ShortSound.java
@@ -7,13 +7,13 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- *
+ * This class represents the model of a ShortSound. A ShortSound essentially consists
+ * of a list of audio tracks (modeled by ShortSoundTrack) and a title. Keep in mind that
+ * any ShortSound models created are going to be synced with the database (ie creating or
+ * modifying a ShortSound will create/update an entry in the database).
  */
 public class ShortSound {
 
-    /**
-     * Local vars
-     */
     private static final String DEFAULT_TITLE = "Untitled";
     private List<ShortSoundTrack> tracks;
     private String title;
@@ -21,31 +21,36 @@ public class ShortSound {
     private static ShortSoundSQLHelper sqlHelper = ShortSoundSQLHelper.getInstance();
 
     /**
-     * Constructor: Create a new empty ShortSound
+     * Create a new empty ShortSound.
+     * @postcondition The newly created ShortSound will be saved in the Database
      */
     public ShortSound() {
         Log.d("DB_TEST", "ShortSound:constructor()");
-        title = DEFAULT_TITLE;  // Default
-        tracks = new ArrayList<ShortSoundTrack>();  // Initially no tracks
+        this.title = DEFAULT_TITLE;  // Default
+        this.tracks = new ArrayList<ShortSoundTrack>();  // Initially no tracks
         this.id = sqlHelper.insertShortSound( this );  // Add ShortSound to the DB
         Log.d("DB_TEST", "Inserted ShortSound: " + this.toString() );
         repInvariant();
     }
 
     /**
-     * Decodes a short sound retrieved form the database and converts into an actual
+     * Decodes a short sound (retrieved from the database) and converts into an actual
      * ShortSound object.
-     * @param map
-     * @return
+     * @param map Key-value pairs corresponding to the fields that make up a ShortSound
+     * @throws AssertionError if an expected key is not found
      */
     public ShortSound( HashMap<String, String> map ) {
+        if ( !map.containsKey( sqlHelper.KEY_ID ) ) throw new AssertionError("Error decoding ShortSound, missing " + sqlHelper.KEY_ID + " field.");
+        if ( !map.containsKey( sqlHelper.KEY_TITLE ) ) throw new AssertionError("Error decoding ShortSound, missing " + sqlHelper.KEY_TITLE + " field.");
         this.id = Long.parseLong( map.get( sqlHelper.KEY_ID ) );
-        this.title = map.get( sqlHelper.KEY_TITLE );
+        this.title = map.get(sqlHelper.KEY_TITLE);
+        this.tracks = new ArrayList<ShortSoundTrack>();
         repInvariant();
     }
 
     /**
-     * Fetch all available ShortSounds (used on app start)
+     * Fetch all available ShortSounds stored in the database.
+     * @return List<ShortSound>
      */
     public static List<ShortSound> getAll() {
         return sqlHelper.queryAllShortSounds();
@@ -59,14 +64,16 @@ public class ShortSound {
     }
 
     /**
-     * Get the tracks for this ShortSound
+     * Get the tracks for this ShortSound.
+     * @return A list of the ShortSoundTracks associated with this ShortSound.
      */
     public List<ShortSoundTrack> getTracks() {
-        return null;
+        return this.tracks;
     }
 
     /**
-     * Add a track to the ShortSound
+     * Add a track to the ShortSound.
+     * @postcondition track will be the last element in the list of tracks.
      */
     public void addTrack( ShortSoundTrack track ) {
         this.tracks.add( track );  // Add track to list
@@ -74,6 +81,7 @@ public class ShortSound {
 
     /**
      * Remove a track from this ShortSound.
+     * @postcondition track will be removed from this ShortSound, the database, and from disk.
      */
     public void removeTrack( ShortSoundTrack track ) {
         this.tracks.remove(track);
@@ -109,6 +117,11 @@ public class ShortSound {
         repInvariant();
     }
 
+    /**
+     * Returns a readable string containing the title of this ShortSound
+     * and the titles of all it's tracks.
+     * @return String
+     */
     @Override
     public String toString() {
         String s = this.title + "[" + this.id + "]{";
@@ -121,25 +134,26 @@ public class ShortSound {
         return s + tracksString + "}";
     }
 
+    /**
+     * Get the id associated with this ShortSound.
+     * @return long
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * This is the representation invarient of a ShortSound model. A key part of this
+     * is making sure the ShortSoundTracks associated with this ShortSound have
+     * corresponding ids.
+     */
     private void repInvariant() {
         if ( this.title == null || !(this.title instanceof String) ) throw new AssertionError("Invalid title");
         if ( this.id < 1 ) throw new AssertionError("Invalid id: " + this.id );
-        if ( !( this.tracks instanceof  ArrayList ) ) throw new AssertionError("List of tracks should be an ArrayList!");
+        for (int i = 0; i < this.tracks.size(); i++) {
+            ShortSoundTrack track = this.tracks.get( i );
+            if ( !( track instanceof  ShortSoundTrack ) ) throw new AssertionError("List of tracks contains an invalid object!");
+            if ( track.getParentId() != this.id ) throw new AssertionError("ShortSoundTrack does not belong to this ShortSound!");
+        }
     }
 }
-
-
-/* ADD THIS TO THE MAIN ACTIVITY FOR TESTING
-
-Log.e("DB_TEST", "MainActivity:onCreate()");
-ShortSound ss = new ShortSound();
-ss.setTitle( "The Best Song Ever" );
-
-ss.addTrack( new ShortSoundTrack( new byte[ShortSoundTrack.BUFFER_SIZE] ) );
-
-ShortSound.getAll();
- */
--- app/src/main/java/com/sloths/speedy/shortsounds/ShortSoundTrack.java
+++ app/src/main/java/com/sloths/speedy/shortsounds/ShortSoundTrack.java
@@ -4,43 +4,56 @@ import java.io.File;
 import java.util.HashMap;
 
 /**
- * Created by neilharlow on 4/17/15.
+ * This class represents the model of a ShortSoundTrack. A ShortSoundTrack essentially
+ * keeps track of an audio file along with any effects that may have been applied to that
+ * file. A ShortSoundTrack should belong to a single ShortSound at any given time.
  */
 public class ShortSoundTrack {
-
-    private static ShortSoundSQLHelper sqlHelper = ShortSoundSQLHelper.getInstance();
 
     public static final String AUDIO_FORMAT = "";  // TODO: format/encoding?
     public static final int TRACK_LENGTH = 30;  // Track length in seconds
     public static final int BUFFER_SIZE = 2000;  // TODO: make buffer size with respect to TRACK_LENGTH
     public static final String DEFAULT_TITLE = "Untitled Track";
 
+    private static ShortSoundSQLHelper sqlHelper = ShortSoundSQLHelper.getInstance();
     private final String originalFile;
     private final String file;
     private long id;
     private String title;
-
+    private final long parentId;
 
     /**
-     * Constructor for a ShortSoundTrack.
+     * Create a ShortSoundTrack provided an existing audio file.
      * @param filename The filename of the recorded audio file.
+     * @param shortSoundId The id of the ShortSound that this track belongs to.
+     * @postcondition This ShortSoundTrack will be stored in the database and a
+     *      copy of the file referenced by filename will be made.
      */
     public ShortSoundTrack( String filename, long shortSoundId ) {
         this.title = DEFAULT_TITLE;
         this.originalFile = filename;
         this.file = filename + "-ss";  // May need to change?
+        this.parentId = shortSoundId;
+        // TODO: create a copy of the original file that will be our "working" copy
         this.id = this.sqlHelper.insertShortSoundTrack( this, shortSoundId );  // Save to the db
     }
 
     /**
      * Construct a ShortSoundTrack from data stored in the DB.
-     * @param map
+     * @param map Key-value pairs corresponding to the fields that make up a ShortSoundTrack.
+     * @throws AssertionError if an expected key is not found.
      */
     public ShortSoundTrack( HashMap<String, String> map ) {
-        this.id = Long.parseLong( map.get( ShortSoundSQLHelper.KEY_ID ) );
-        this.file = map.get( ShortSoundSQLHelper.KEY_TRACK_FILENAME_MODIFIED );
-        this.originalFile = map.get( ShortSoundSQLHelper.KEY_TRACK_FILENAME_ORIGINAL );
-        this.title = map.get( ShortSoundSQLHelper.KEY_TITLE );
+        if ( !map.containsKey( sqlHelper.KEY_ID ) ) throw new AssertionError("Error decoding ShortSoundTrack, missing " + sqlHelper.KEY_ID + " field.");
+        if ( !map.containsKey( sqlHelper.KEY_TRACK_FILENAME_ORIGINAL ) ) throw new AssertionError("Error decoding ShortSoundTrack, missing " + sqlHelper.KEY_TRACK_FILENAME_ORIGINAL + " field.");
+        if ( !map.containsKey( sqlHelper.KEY_TRACK_FILENAME_MODIFIED ) ) throw new AssertionError("Error decoding ShortSoundTrack, missing " + sqlHelper.KEY_TRACK_FILENAME_MODIFIED + " field.");
+        if ( !map.containsKey( sqlHelper.KEY_TITLE ) ) throw new AssertionError("Error decoding ShortSoundTrack, missing " + sqlHelper.KEY_TITLE + " field.");
+        if ( !map.containsKey( sqlHelper.KEY_SHORT_SOUND_ID ) ) throw new AssertionError("Error decoding ShortSoundTrack, missing " + sqlHelper.KEY_SHORT_SOUND_ID + " field.");
+        this.id = Long.parseLong( map.get( sqlHelper.KEY_ID ) );
+        this.file = map.get( sqlHelper.KEY_TRACK_FILENAME_MODIFIED );
+        this.originalFile = map.get( sqlHelper.KEY_TRACK_FILENAME_ORIGINAL );
+        this.title = map.get( sqlHelper.KEY_TITLE );
+        this.parentId = Long.parseLong( map.get( sqlHelper.KEY_SHORT_SOUND_ID ) );
     }
 
     public void addEffect() {
@@ -54,6 +67,8 @@ public class ShortSoundTrack {
     /**
      * Remove the ShortSoundTrack from the database and delete the corresponding
      * files.
+     * @postcondition This track will no longer exist in the database, and its audio files
+     *      will be removed.
      */
     public void delete() {
         sqlHelper.removeShortSoundTrack( this );
@@ -75,19 +90,35 @@ public class ShortSoundTrack {
         }
     }
 
-    public void setId( long id ) {
-        this.id = id;
-    }
-
+    /**
+     * Get the title of this ShortSoundTrack.
+     * @return String
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Returns the title of this ShortSoundTrack
+     * @return String
+     */
     @Override
     public String toString() {
         return this.title;
     }
 
+    /**
+     * Get the parentId (ShortSound id) that this track is associated with.
+     * @return long
+     */
+    public long getParentId() {
+        return this.parentId;
+    }
+    /**
+     * This is the representation invarient of the ShortSoundTrack model.
+     * The main thing here is that a ShortSoundTrack becomes invalid if the files
+     * associated with the tracks are non-existent.
+     */
     private void repInvariant() {
         if ( this.title == null || !(this.title instanceof String) ) throw new AssertionError("Invalid title");
         if ( this.file == null || !(this.file instanceof String) ) throw new AssertionError("Invalid filename");
