819e4d23732df90ab2a7d60aa5b1eb2bf5b7637d
Justin Yoon <justin.o.yoon@gmail.com>
2015-05-15 20:34:22 -0700

    Some more tests in ShortSoundTrack.

--- app/src/androidTest/java/com/sloths/speedy/shortsounds/ShortSoundTrackTest.java
+++ app/src/androidTest/java/com/sloths/speedy/shortsounds/ShortSoundTrackTest.java
@@ -2,6 +2,7 @@ package com.sloths.speedy.shortsounds;
 
 import android.test.AndroidTestCase;
 
+import com.sloths.speedy.shortsounds.model.ShortSoundSQLHelper;
 import com.sloths.speedy.shortsounds.model.ShortSoundTrack;
 
 import java.io.File;
@@ -14,9 +15,9 @@ import java.util.HashMap;
  */
 public class ShortSoundTrackTest extends AndroidTestCase {
 
-/*
- * Constructor
- */
+    /*
+     * Constructor
+     */
 
     /**
      * Tests that a new ShortSoundTrack that was constructed with the
@@ -24,6 +25,7 @@ public class ShortSoundTrackTest extends AndroidTestCase {
      */
     public void testNewShortSoundTrackHasProperInitialFields() {
         ShortSoundTrack test = new ShortSoundTrack(new File(""), 0);
+
         assert(test.getFileName() != null);
         assert(test.getId() >= 0);
         assert(test.getTitle() != null);
@@ -43,5 +45,43 @@ public class ShortSoundTrackTest extends AndroidTestCase {
         } catch (AssertionError error) {
             assert(true);
         }
+    }
+
+    /**
+     * Tests that a new ShortSoundTrack that was constructed with the single parameter
+     * constructor gets the correct values for its fields.
+     */
+    public void testNewShortSoundTrackGetsFieldsFromSQL() {
+        HashMap<String, String> testValues = new HashMap<String, String>();
+
+        testValues.put(ShortSoundSQLHelper.KEY_ID, "0");
+        testValues.put(ShortSoundSQLHelper.KEY_TRACK_FILENAME_MODIFIED, "test-file-modified");
+        testValues.put(ShortSoundSQLHelper.KEY_TITLE, "TestTrack");
+        testValues.put(ShortSoundSQLHelper.KEY_SHORT_SOUND_ID, "0");
+        testValues.put(ShortSoundSQLHelper.EQ_EFFECT_PARAMS, null);
+        testValues.put(ShortSoundSQLHelper.REVERB_EFFECT_PARAMS, null);
+
+        ShortSoundTrack test = new ShortSoundTrack(testValues);
+
+        assert(test.getFileName().equals("test-file-modified"));
+        assert(test.getId() == 0);
+        assert(test.getTitle().equals("TestTrack"));
+        assert(test.getParentId() == 0);
+        assert(test.getmEqEffect() != null);
+        assert(test.getmReverbEffect() != null);
+    }
+
+    /*
+     * getTitle
+     */
+
+    /**
+     * Tests that when constructed with the two parameter constructor, getTitle returns the default
+     * title.
+     */
+    public void testDefaultTitle() {
+        ShortSoundTrack test = new ShortSoundTrack(new File(""), 0);
+
+        assert(test.getTitle().equals(ShortSoundTrack.DEFAULT_TITLE));
     }
 }
