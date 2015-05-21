aaf638d9d76d7e6551a7f8fe2cde43e80f5f8a5e
caseympfischer <caseympfischer@gmail.com>
2015-05-21 15:04:42 -0700

    initial implementation of GraphShape (for EqEffect)

--- app/src/main/java/com/sloths/speedy/shortsounds/model/EqEffect.java
+++ app/src/main/java/com/sloths/speedy/shortsounds/model/EqEffect.java
@@ -4,6 +4,8 @@ import android.graphics.PointF;
 import android.media.audiofx.Equalizer;
 import android.util.Log;
 
+import java.util.Arrays;
+
 /**
  * Equalizer Effect. This class provides a wrapper around the existing Android Equalizer
  * effect class.
@@ -20,6 +22,8 @@ public class EqEffect extends Effect {
     public static final float DEFAULT_X2 = 0.7f;
     private PointF[] eqPoints;
     private boolean isActive;
+
+    private static final String TAG = "EFFECTS-PLAYBACK";
 
     // Constructor used when loading a track from a recorded file
     public EqEffect() {
@@ -62,10 +66,12 @@ public class EqEffect extends Effect {
      * Set the properties of the Equalizer effect class.
      */
     private void setEffectProperties() {
-        short bandLevels[] = convertParamsToSettings();
         Equalizer eq = (Equalizer) effect;
+        short bandLevels[] = convertParamsToSettings(eq);
+
         for (int i = 0; i < eq.getNumberOfBands(); i++) {
             Log.d("EFFECTS", "set band["+i+"] to level[-1500], previous level["+eq.getBandLevel((short)i)+"]");
+            int[] range = eq.getBandFreqRange((short) i);
             eq.setBandLevel( (short)i, bandLevels[i] );
         }
     }
@@ -74,15 +80,136 @@ public class EqEffect extends Effect {
      * Convert the EqEffect parameters into band levels that are understood by the effects engine.
      * @return an array containing the corresponding band levels
      */
-    private short[] convertParamsToSettings() {
+    private short[] convertParamsToSettings(Equalizer eq) {
         // TODO
+        // may want to pass in eq.getNumberOfBands() instead of trying to set that
+        // via Equalizer.Settings
         short bands[] = new short[5];
         bands[0] = (short)-1500;
         bands[1] = (short)-1500;
         bands[2] = (short)-1500;
         bands[3] = (short)-1500;
         bands[4] = (short)-1500;
+
+        // these are the x values of all eq points (including non-visible guide points),
+        // in order of ascending x
+        float[] allPointsXVals = {
+                Math.max((float) (eqPoints[0].x - 0.125), 0f),
+                eqPoints[0].x,
+                Math.min((float) (eqPoints[0].x + 0.125), 1f),
+                Math.max((float) (eqPoints[1].x - 0.125), 0f),
+                eqPoints[1].x,
+                Math.min((float) (eqPoints[1].x + 0.125), 1f)
+        };
+        Arrays.sort(allPointsXVals);
+
+        // TODO: hook this part up to the GraphShape business
+        for (short i = 0; i < bands.length; i++) {
+            // We're modeling the EQ settings as triangles whose peaks/troughs are
+            // the eqPoints.  For each band, we calculate the area under that triangle
+            // and set the band level (height) such that the band's area equals the area
+            // under that curve.
+            //int[] bounds = eq.getBandFreqRange(i);
+            //short triangle1Width = Math.max(0, )
+        }
         return bands;
+    }
+
+    // used for calculating band levels.
+    private class GraphShape {
+        private float x1, y1, x2, y2, slope;
+        public GraphShape(float x1, float y1, float x2, float y2) {
+            // disallow crossing the x axis
+            if ((y1 > 0 && y2 < 0) || (y1 < 0 && y2 > 0)) {
+                throw new IllegalArgumentException("GraphShapes cannot cross the x axis.");
+            }
+            // just to be sure we have ascending x values
+            if (x1 <= x2) {
+                this.x1 = x1;
+                this.y1 = y1;
+                this.x2 = x2;
+                this.y2 = y2;
+            } else {
+                this.x1 = x2;
+                this.y1 = y2;
+                this.x2 = x1;
+                this.y2 = y1;
+            }
+            if (this.x1 == this.x2) {
+                this.slope = 0;
+            } else {
+                this.slope = (y2 - y1) / (x2 - x1);
+            }
+        }
+
+        public float getArea(float lowBound, float highBound) {
+            if (lowBound > highBound) {
+                throw new IllegalArgumentException("lowBound cannot be greater than highBound.");
+            }
+            float innerX1 = Math.min(Math.max(lowBound, x1), highBound);
+            float innerX2 = Math.max(Math.min(highBound, x2), lowBound);
+            float innerY1;
+            float innerY2;
+            if (innerX1 == innerX2) {
+                return 0f;
+            }
+            innerY1 = y1 + (slope * (innerX1 - x1));
+            innerY2 = y2 + (slope * (innerX2 - x2));
+
+            // rectangular portion
+            float retVal = (innerX2 - innerX1) * innerY1;
+            // triangular portion
+            retVal += (innerX2 - innerX1) * (innerY2 - innerY1) / 2;
+            return retVal;
+        }
+
+        public float getIntersectArea(GraphShape other, float lowBound, float highBound) {
+            if (this.x1 > other.x2 || this.x2 < other.x1
+                    || this.getSign() != other.getSign()) {
+                return 0f;
+            }
+            // using y=mx+b form
+            float b1 = this.getYatX(0);
+            float b2 = other.getYatX(0);
+            float intersectX;
+            float intersectY;
+            if (this.slope == other.slope) {
+                // in this case we don't care about the point of intersection
+                intersectX = -1;
+                intersectY = 0;
+            } else {
+                // modeling the summed line of the two shapes' outer bounds
+                float sumB = b1 + b2;
+                float sumSlope = this.slope + other.slope;
+                intersectX = -1 * sumB / sumSlope;
+                intersectY = this.getYatX(intersectX);
+            }
+            float innerX1 = Math.min(Math.max(lowBound, Math.max(this.x1, other.x1)), highBound);
+            float innerY1 = Math.min(Math.abs(this.getYatX(innerX1)), Math.abs(other.getYatX(innerX1)))
+                    * this.getSign();
+            float innerX2 = Math.max(Math.min(highBound, Math.min(this.x2, other.x2)), lowBound);
+            float innerY2 = Math.min(Math.abs(this.getYatX(innerX2)), Math.abs(other.getYatX(innerX2)))
+                    * this.getSign();
+            if (intersectX > innerX1 && intersectX < innerX2) {
+                GraphShape g1 = new GraphShape(innerX1, innerY1, intersectX, intersectY);
+                GraphShape g2 = new GraphShape(intersectX, intersectY, innerX2, innerY2);
+                return g1.getArea(lowBound, highBound) + g2.getArea(lowBound, highBound);
+            } else {
+                GraphShape g = new GraphShape(innerX1, innerY1, innerX2, innerY2);
+                return g.getArea(lowBound, highBound);
+            }
+        }
+
+        private float getYatX(float x) {
+            return this.y1 - (this.slope * (this.x1 - x));
+        }
+        private int getSign() {
+            if (y1 > 0 || y2 > 0 || (y1 == 0 && y2 == 0)) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
     }
 
     // Stored in Track table for effect params and on off
