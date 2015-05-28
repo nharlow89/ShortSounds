70e1ed23615b37c5cb19be5456a343b44aa83582
Mattie <mattie.carlson32@gmail.com>
2015-05-28 13:59:16 -0700

    Commented SwipeToDeleteListener

--- app/src/main/java/com/sloths/speedy/shortsounds/view/SwipeToDeleteListener.java
+++ app/src/main/java/com/sloths/speedy/shortsounds/view/SwipeToDeleteListener.java
@@ -4,6 +4,14 @@ package com.sloths.speedy.shortsounds.view;
  * Simple interface for event handlers in the TrackSwipeListener.
  */
 public interface SwipeToDeleteListener {
+
+    /**
+     * Handles the event when a track is deleted
+     */
     public void onTrackDelete();
+
+    /**
+     * Handles the event when a track title is edited
+     */
     public void onEditTrackTitle();
 }
